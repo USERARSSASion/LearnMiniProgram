9e5c719f7cef6a97b15fbafaba861f8fa307c418
USERARSSASion <178453765@qq.com>
2020-06-20 19:21:07 +0800

    基础

---
+++ Pages/home/home.js
@@ -0,0 +1,12 @@
+// getApp() 获取 App() 产生的实例对象
+const app = getApp();
+const name = app.globalData.name;
+
+Page({
+  data: {
+    name: '明',
+  },
+  handleGetUserInfo(event) {
+    console.log(event)
+  }
+})
